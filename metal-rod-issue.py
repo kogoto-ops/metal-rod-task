f9089d6e49c886b54734e7c2fc448f30ef23bec6
Lizaveta <st146606@student.spbu.ru>
2025-11-11 09:37:18 +0000

    Liza's changes

--- metal-rod-issue.py
+++ metal-rod-issue.py
@@ -6,9 +6,9 @@ def best_rod_cut_value(total_rod_length, price_arr):
     # fill and replace the values or the lists above
     for current_rod_length in range(1, total_rod_length + 1):
         for trial_cut_length in range(1, current_rod_length + 1):
-            if trial_cut_length <= len(price_list):
+            if trial_cut_length <= len(price_arr):
                 # Compute total price if we cut at this length
-                potential_price = (price_list[trial_cut_length - 1] + max_price_for_length[current_rod_length - trial_cut_length])
+                potential_price = (price_arr[trial_cut_length - 1] + max_price_for_length[current_rod_length - trial_cut_length])
                 # Update if this cut gives a better total price
                 if potential_price > max_price_for_length[current_rod_length]:
                     max_price_for_length[current_rod_length] = potential_price
@@ -22,8 +22,3 @@ def best_rod_cut_value(total_rod_length, price_arr):
         remaining_length -= best_first_cut_length[remaining_length]
 
     return optimal_cut_plan, max_price_for_length[-1]
-
-
-  
-
-  
